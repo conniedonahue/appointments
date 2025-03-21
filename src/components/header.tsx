013c5d056ea583f3778263fdc66e301989fc4903
conniedonahue <connordonahue09@gmail.com>
2025-03-20 21:50:37 -0400

    initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="w-full fixed top-0 z-10">
+      <nav className="bg-orange-300 text-orange-50 container relative flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href="/" className="text-3xl font-bold hover:opacity-8">
+          Home
+        </Link>
+        <div className="mr-5 space-x-4 text-xl">
+          <Link href="/about" className="hover:underline">
+            About Us
+          </Link>
+          <Link href="/providers" className="hover:underline">
+            Providers
+          </Link>
+          <Link href="/signin" className="hover:underline">
+            Sign in
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
